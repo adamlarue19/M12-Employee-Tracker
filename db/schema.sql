e020e00984323c7cf09967602b18e7072488a2bb
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2022-12-06 19:23:36 -0500

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -11,9 +11,13 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    department_id INT,
-    salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
 );
